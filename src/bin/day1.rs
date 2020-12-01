5364388b88b1fcb2e60732d4f420c8db02829514
jayvalentine <jayv136@gmail.com>
2020-12-01 16:50:01 +0000

    Added basic find_match function

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -3,12 +3,18 @@
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn example() {
         let n = vec![1721, 979, 366, 299, 675, 1456];
 
-        assert_eq!(514579, find_match(n));
+        assert_eq!(514579, find_match(&n));
     }
+}
+
+fn find_match(n: &[u32]) -> u32 {
+    return 0;
 }
 
 fn main() {
