dcd01a5a5dd343f7b6d3c5e2ed3d31b5ad442609
jayvalentine <jayv136@gmail.com>
2020-12-01 16:58:47 +0000

    Initial implementation of pair-finding

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,6 +1,8 @@
 // Advent of Code 2020
 // Day 1
 
+use std::collections::HashMap;
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -13,7 +15,31 @@ mod tests {
     }
 }
 
+// Given an array of numbers, find the two numbers that sum to 2020
+// and return their product.
 fn find_match(n: &[u32]) -> u32 {
+    let mut map: HashMap<u32, bool> = HashMap::new();
+
+    // Put each number into the hashmap.
+    for i in n {
+        map.insert(*i, true);
+    }
+
+    // Now iterate over each key in the hashmap.
+    // For each key, check to see if it's opposite
+    // (i.e. the value with which it sums to 2020)
+    // exists.
+    //
+    // If so, multiply them and return.
+    for (k, _v) in &map {
+        let left = *k;
+        let right = 2020 - left;
+
+        if map.contains_key(&right) {
+            return left * right;
+        }
+    }
+
     return 0;
 }
 
